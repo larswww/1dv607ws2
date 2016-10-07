ca769141499e2b83395a51e8e1513acec8f29a93
Anders Melin <am222vp@student.lnu.se>
2016-10-07 13:46:26 +0545

    ...

--- view/view.php
+++ view/view.php
@@ -23,6 +23,7 @@ class MainLayout {
   private $title = "Välkommen till båtklubben den lila illern";
 
   public function __construct($partial){
+    echo "partial is:" . $partial;
     $this->maincontent = $partial->show();
   }
 
