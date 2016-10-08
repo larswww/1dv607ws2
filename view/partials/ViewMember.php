9801f4edc65fe76af3dd0bfd8ecb04154b335e7b
Anders Melin <am222vp@student.lnu.se>
2016-10-08 10:38:19 +0545

    ...

--- view/partials/ViewMember.php
+++ view/partials/ViewMember.php
@@ -11,6 +11,7 @@ class ViewMember{
     $member = $memberAndBoats[0]['member'];
     $boats = $memberAndBoats[0]['boats'];
     print_r($boats);
+    // Array ( [0] => Array ( [type] => Other [0] => Other [length] => 0 [1] => 0 [ID] => 57f866c368f93 [2] => 57f866c368f93 [ownerID] => 57f857c764771 [3] => 57f857c764771 )
 
     $this->memberId = $member['ID'];
     $this->firstname = $member['firstName'];
@@ -20,20 +21,25 @@ class ViewMember{
   }
 
   private function boatlist(){
+    $boats = $this->boats;
 
     if(!$this->boats){
       return "No boats";
     }
 
     $str = "<dl>";
-    foreach ($this->boats as $key => $value) {
+    for($i = 0; i < count($boats); i++){
+      $id = $boats[$i]['ID'];
+      $type = $boats[$i]['type'];
+      $length = $boats[$i]['length'];
+
       $str .= "
-      <dt>Boat {$key}
-        (<a href='&action=editBoat&boatId={$value->boatId}'>edit</a>
-        <a href='&action=deleteBoat&boatId={$value->boatId}'>Delete</a>)
+      <dt>Boat {$i}:<br>
+        <a href='&action=editBoat&boatId={$id}'>edit</a><br>
+        <a href='&action=deleteBoat&boatId={$id}'>Delete</a>
       </dt>
-      <dd>Type:&nbsp;&nbsp;&nbsp;{$value->type}</dd>
-      <dd>Length: {$value->length}</dd>
+      <dd>Type:&nbsp;&nbsp;&nbsp;{$type}</dd>
+      <dd>Length: {$length}</dd>
       ";
     }
     return $str . "</dl>";
