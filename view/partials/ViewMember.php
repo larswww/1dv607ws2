e8809e4e6ce10e40f02610f464de30a715b970f4
Anders Melin <am222vp@student.lnu.se>
2016-10-08 10:49:02 +0545

    ...

--- view/partials/ViewMember.php
+++ view/partials/ViewMember.php
@@ -21,32 +21,32 @@ class ViewMember{
   }
 
   private function boatlist(){
-    $boats = $this->boats;
-
-    if(!$this->boats){
-      return "No boats";
-    }
-
-    $str = "<dl>";
-    for($i = 0; i < count($boats); i++){
-      $id = $boats[$i]['ID'];
-      $type = $boats[$i]['type'];
-      $length = $boats[$i]['length'];
-
-      $str .= "
-      <dt>Boat {$i}:<br>
-        <a href='&action=editBoat&boatId={$id}'>edit</a><br>
-        <a href='&action=deleteBoat&boatId={$id}'>Delete</a>
-      </dt>
-      <dd>Type:&nbsp;&nbsp;&nbsp;{$type}</dd>
-      <dd>Length: {$length}</dd>
-      ";
-    }
-    return $str . "</dl>";
+    // $boats = $this->boats;
+    //
+    // if(!$this->boats){
+    //   return "No boats";
+    // }
+    //
+    // $str = "<dl>";
+    // for($i = 0; i < count($boats); i++){
+    //   $id = $boats[$i]['ID'];
+    //   $type = $boats[$i]['type'];
+    //   $length = $boats[$i]['length'];
+    //
+    //   $str .= "
+    //   <dt>Boat {$i}:<br>
+    //     <a href='&action=editBoat&boatId={$id}'>edit</a><br>
+    //     <a href='&action=deleteBoat&boatId={$id}'>Delete</a>
+    //   </dt>
+    //   <dd>Type:&nbsp;&nbsp;&nbsp;{$type}</dd>
+    //   <dd>Length: {$length}</dd>
+    //   ";
+    // }
+    // return $str . "</dl>";
   }
 
   public function show(){
-    $boatlist = $this->boatlist();
+    //$boatlist = $this->boatlist();
     return "
     <div>
       <span>name: {$this->firstname} {$this->lastname}</span>
